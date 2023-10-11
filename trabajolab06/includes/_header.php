3d5e2639dc03b2cd9e9158ef171ff1a53aeba335
Saraicht <144289425+Saraicht@users.noreply.github.com>
2023-10-11 17:48:10 -0500

    Add files via upload

---
+++ trabajolab06/includes/_header.php
@@ -0,0 +1,77 @@
+<?php
+error_reporting(0);
+session_start();
+$actualsesion = $_SESSION['correo'];
+
+if($actualsesion == null || $actualsesion == ''){
+
+    echo 'acceso denegado';
+    die();
+}
+?>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Base de datos</title>
+
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
+    <link href="https://fonts.googleapis.com/css2?family=Material+Icons" rel="stylesheet">
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    
+    <link rel="stylesheet" href="../../css/styles.css">
+    <link href="../../css/sb-admin-2.min.css" rel="stylesheet">
+   
+</head>
+<body>
+    
+<div id="wrapper">
+   
+    <ul class="navbar-nav bg-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.php">
+    <div class="sidebar-brand-icon rotate-n-15">
+    </div>
+
+</a>
+<hr class="sidebar-divider my-0">
+
+
+
+<li class="nav-item">
+    <a class="nav-link collapsed" href="index.php">
+    <span class="material-icons">pattern</span>
+        <span>Productos</span>
+    </a>
+</li>
+
+<hr class="sidebar-divider">
+<div class="sidebar-heading">
+    PERFIL
+</div>
+<li class="nav-item">
+    <a class="nav-link collapsed" href="usuariosindex.php">
+        <span class="material-icons">people</span>
+        <span>Información usuario</span>
+    </a>
+</li>
+<li class="nav-item">
+    <a class="nav-link" href="../../includes/_sesion/cerrarSesion.php">
+    <span class="material-icons">logout</span>
+        <span>Salir</span></a>
+        
+</li>
+
+<hr class="sidebar-divider d-none d-md-block">
+
+<div class="text-center d-none d-md-inline">
+    <button class="rounded-circle border-0"></button>
+</div>
+</ul>
+
+       <div id="content-wrapper" class="d-flex flex-column">
+      
+                
+
+
